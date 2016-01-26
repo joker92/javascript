50c239d64a70efd9cfb26b237a41e48755e087f5
joker92 <euroseuff@gmail.com>
2016-01-26 14:11:38 +0100

    new file

---
+++ 1/script/codice.js
@@ -0,0 +1,11 @@
+function Salutami(){
+	alert("CIAO KIKKO!!!");
+}
+ var testo ="il mio testo";
+  console.log(typeof(testo));
+ console.log(testo+3);
+ testo=2;
+ console.log(testo+3);
+ testo=true;
+ console.log(typeof(testo));
+ console.log(testo+1);
